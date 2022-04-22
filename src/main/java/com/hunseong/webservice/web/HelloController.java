ddc7c881c7c1408203c982a4eed14086764ff117
Hunseong <phunseong@gmail.com>
2022-04-22 19:17:42 +0900

    HelloResponseDto return Controller test

--- src/main/java/com/hunseong/webservice/web/HelloController.java
+++ src/main/java/com/hunseong/webservice/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.hunseong.webservice.web;
 
+import com.hunseong.webservice.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,5 +14,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
