651da9d45c3ec6daff3c4cf9f28402f77e7d5c97
Hunseong <phunseong@gmail.com>
2022-04-22 22:12:06 +0900

    mustach index page

---
+++ src/main/java/com/hunseong/webservice/web/IndexController.java
@@ -0,0 +1,16 @@
+package com.hunseong.webservice.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by Hunseong on 2022/04/22
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
