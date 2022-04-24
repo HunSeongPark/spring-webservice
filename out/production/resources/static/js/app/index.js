cfd0d9e719b11cc9b123c1944c68bad34878750c
Hunseong <phunseong@gmail.com>
2022-04-24 17:08:59 +0900

    posts delete

--- out/production/resources/static/js/app/index.js
+++ out/production/resources/static/js/app/index.js
@@ -9,9 +9,9 @@ var main = {
             _this.update();
         });
 
-        // $('#btn-delete').on('click', function () {
-        //     _this.delete();
-        // });
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        });
     },
     save : function () {
         var data = {
@@ -54,21 +54,21 @@ var main = {
             alert(JSON.stringify(error));
         });
     },
-    // delete : function () {
-    //     var id = $('#id').val();
-    //
-    //     $.ajax({
-    //         type: 'DELETE',
-    //         url: '/api/v1/posts/'+id,
-    //         dataType: 'json',
-    //         contentType:'application/json; charset=utf-8'
-    //     }).done(function() {
-    //         alert('글이 삭제되었습니다.');
-    //         window.location.href = '/';
-    //     }).fail(function (error) {
-    //         alert(JSON.stringify(error));
-    //     });
-    // }
+    delete : function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8'
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    }
 
 };
 
--- src/main/java/com/hunseong/webservice/web/PostApiController.java
+++ src/main/java/com/hunseong/webservice/web/PostApiController.java
@@ -25,12 +25,17 @@ public class PostApiController {
 
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
-        log.info("update logic");
         return postsService.update(id, requestDto);
     }
 
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
     }
 }
