f07e77a77a17f92ba4a209dde9ac80e38960b3a0
Hunseong <phunseong@gmail.com>
2022-04-24 19:12:19 +0900

    ArgumentResolver를 통한 SessionUser 보일러 플레이트 코드 제거

---
+++ src/main/java/com/hunseong/webservice/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,36 @@
+package com.hunseong.webservice.config.auth;
+
+import com.hunseong.webservice.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by Hunseong on 2022/04/24
+ */
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("user");
+    }
+}
--- src/main/java/com/hunseong/webservice/web/IndexController.java
+++ src/main/java/com/hunseong/webservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.hunseong.webservice.web;
 
+import com.hunseong.webservice.config.auth.LoginUser;
 import com.hunseong.webservice.config.auth.dto.SessionUser;
 import com.hunseong.webservice.service.posts.PostsService;
 import com.hunseong.webservice.web.dto.PostsResponseDto;
@@ -19,13 +20,10 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
