256e11de86ddac24d29c99a2ac5685196a992cb4
Hunseong <phunseong@gmail.com>
2022-04-25 00:44:20 +0900

    OAuth - naver login

--- src/main/java/com/hunseong/webservice/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/hunseong/webservice/config/auth/dto/OAuthAttributes.java
@@ -32,11 +32,35 @@ public class OAuthAttributes {
     public static OAuthAttributes of(String registrationId, String userNameAttributeName,
                                      Map<String, Object> attributes) {
 
+        // Naver Login
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
+        // Google Login
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
